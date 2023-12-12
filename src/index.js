2668a4023a1a16d9388781766f823cb7e51746d4
kevin.lee5 <kevin.lee5@cs.ucalgary.ca>
2023-12-12 15:17:47 -0700

    Created a Todo list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+import { useState } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <>
+      <Todos todos = {todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick = {increment}>+</button>
+      </div>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +35,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
